da277a62e3ced8fe2ce18fcf93086c20e24894d4
anu-devi02002 <101132692+anu-devi02002@users.noreply.github.com>
2022-03-08 12:24:17 +0530

    Update data.py

--- data.py
+++ data.py
@@ -3,30 +3,40 @@ import threading
 import time
 import datetime
 from yaml.loader import SafeLoader
-def TimeFunction(cont,t):
-    print("current time:-",datetime.datetime.now())    
+st=[]
+def TimeFunction(cont,t,tt):
+    print(datetime.datetime.now(),tt)    
     time.sleep(t)
-    print("current time:-",datetime.datetime.now())  
+    print(datetime.datetime.now(),tt)  
 
-def flow(dic):
-    print(dic['Activities'])
-    kk=list(dic['Activities'].keys())
-
-    for i in range(dic):
-        print(i)
-        if type=='flow':
-            flow(i)
+def flow(dic,ex):
+    d=dic["Activities"]
+    kk=list(d.keys())
+    n=-1
+    for i in d.values():
+        n+=1
+        if i['Type']=='Flow':
+            st.append(kk[0])
+            print(datetime.datetime.now(),end=";")
+            for i in st:
+                print(i,end=".")
+            print("entry")
+            flow(i,i['Execution'])
+            print(datetime.datetime.now(),kk[0],'exit') 
         else:
-            TimeFunction(dic[inputs['Function']],dic[inputs['ExecutionTime']])
+            print(datetime.datetime.now(),kk[n])  
+            TimeFunction(i['Inputs']['FunctionInput'],int(i['Inputs']['ExecutionTime']),kk[n])
+
 # Open the file and load the file
 with open('Milestone1_Example.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
-    k=list(data.keys())
-    print(k[0])
-    print(data[k[0]])
-    for i in data[k[0]]:
-        print(i)
-        if type=='flow':
-            flow(i)
-    
+    kk=list(data.keys())
+    print(kk[0])
+    for i in data.values():
+        print(i['Type'])
+        if i['Type']=='Flow':
+            st.append(kk[0])
+            print(datetime.datetime.now(),kk[0],'entry')  
+            flow(i,i['Execution'])
+            print(datetime.datetime.now(),kk[0],'exit') 
     
