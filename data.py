38ac887332c16080b5e91a27fa814b8f2b4cf5b2
anu-devi02002 <101132692+anu-devi02002@users.noreply.github.com>
2022-03-08 13:54:45 +0530

    Update data.py

--- data.py
+++ data.py
@@ -3,11 +3,22 @@ import threading
 import time
 import datetime
 from yaml.loader import SafeLoader
+fi=open("log_file.txt","w+")
 st=[]
-def TimeFunction(cont,t,tt):
-    print(datetime.datetime.now(),tt)    
+def TimeFunction(cont,t):
+    s1=str(datetime.datetime.now())
+    s1+=";"
+    for e in range(len(st)):
+        if e==len(st)-1:
+            s1+=st[e]
+        else:
+            s1+=st[e]
+            s1+="."
+    s1+=" Executing TimeFunction ("
+    s1=s1+cont+", "+str(t)+")"
+    fi.writelines(s1+'\n') 
     time.sleep(t)
-    print(datetime.datetime.now(),tt)  
+ 
 
 def flow(dic,ex):
     d=dic["Activities"]
@@ -16,27 +27,81 @@ def flow(dic,ex):
     for i in d.values():
         n+=1
         if i['Type']=='Flow':
-            st.append(kk[0])
-            print(datetime.datetime.now(),end=";")
-            for i in st:
-                print(i,end=".")
-            print("entry")
+            st.append(kk[n])
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Entry"
+            fi.writelines(s1+'\n')
             flow(i,i['Execution'])
-            print(datetime.datetime.now(),kk[0],'exit') 
-        else:
-            print(datetime.datetime.now(),kk[n])  
-            TimeFunction(i['Inputs']['FunctionInput'],int(i['Inputs']['ExecutionTime']),kk[n])
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Exit"
+            fi.write(s1+'\n')
+            st.remove(kk[n])
+        else: 
+            st.append(kk[n])
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Entry"
+            fi.writelines(s1+'\n')
+            TimeFunction(i['Inputs']['FunctionInput'],int(i['Inputs']['ExecutionTime']))
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Exit"
+            fi.write(s1+'\n')
+            st.remove(kk[n])
 
 # Open the file and load the file
 with open('Milestone1_Example.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
     kk=list(data.keys())
-    print(kk[0])
     for i in data.values():
-        print(i['Type'])
         if i['Type']=='Flow':
             st.append(kk[0])
-            print(datetime.datetime.now(),kk[0],'entry')  
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Entry"
+            fi.writelines(s1+'\n')
             flow(i,i['Execution'])
-            print(datetime.datetime.now(),kk[0],'exit') 
+            s1=str(datetime.datetime.now())
+            s1+=";"
+            for e in range(len(st)):
+                if e==len(st)-1:
+                    s1+=st[e]
+                else:
+                    s1+=st[e]
+                    s1+="."
+            s1+=" Exit"
+            fi.write(s1)
+            st.remove(kk[0])
     
