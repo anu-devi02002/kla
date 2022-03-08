760bbb21dfdea92ac4f312f0b8fcdd927a0efe11
anu-devi02002 <101132692+anu-devi02002@users.noreply.github.com>
2022-03-08 11:16:00 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,52 @@
+import yaml 
+import threading
+import time
+import datetime
+from yaml.loader import SafeLoader
+def TimeFunction(cont,t):
+    print("current time:-",datetime.datetime.now())    
+    time.sleep(t)
+    print("current time:-",datetime.datetime.now())  
+
+def flow(dic):
+    print(dic['Activities'])
+    kk=list(dic['Activities'].keys())
+
+    for i in range(dic):
+        print(i)
+        if type=='flow':
+            flow(i)
+        else:
+            TimeFunction(dic[inputs['Function']],dic[inputs['ExecutionTime']])
+# Open the file and load the file
+with open('Milestone1_Example.yaml') as f:
+    data = yaml.load(f, Loader=SafeLoader)
+    k=list(data.keys())
+    print(k[0])
+    print(data[k[0]])
+    for i in data[k[0]]:
+        print(i)
+        if type=='flow':
+            flow(i)
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+'''for p_id, p_info in people.items():
+    print("\nPerson ID:", p_id)
+    
+    for key in p_info:
+        print(key + ':', p_info[key])'''
+    
+    
